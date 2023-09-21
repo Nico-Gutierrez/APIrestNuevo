4c660f44a5818b4f9223189fef24a20fc3a3a614
Nicolas <nicolasguti200412@gmail.com>
2023-09-21 01:45:45 -0500

    conf(Backend): 3.5

---
+++ APIrestNuevo/Repository/RepositoryContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entites.Models;
+using Repository.Configuration;
+
+namespace Repository;
+public class RepositoryContext : DbContext 
+    { 
+    public RepositoryContext(DbContextOptions options) 
+        : base(options) 
+    { 
+    } 
+    
+    public DbSet<Company> Companies { get; set; } 
+    public DbSet<Employee> Employees { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new CompanyConfiguration());
+        modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+
+    }
+}
